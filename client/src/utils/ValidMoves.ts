480212bd9544d547b1987ff7d6b53d0ed276cda6
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2021-08-09 14:17:24 +0530

    all validmoves destruction and creation is working properly, testing is remaining

--- client/src/utils/ValidMoves.ts
+++ client/src/utils/ValidMoves.ts
@@ -47,12 +47,24 @@ class ValidMoves {
     this.currentGameState[moveMade[1][0]][moveMade[1][1]] = PegTypes.EmptySlot;
     this.currentGameState[moveMade[2][0]][moveMade[2][1]] = PegTypes.FilledSlot;
 
-    this.handleDropSlotCreation(moveMade);
-    // this.handleFilledSlotCreation(moveMade);
+    const m1 = this.handleDropSlotCreation(moveMade);
+    const m2 = this.handleFilledSlotCreation(moveMade);
+
+    m1.forEach((m) => {
+      this.validMoves.push(m);
+    });
+
+    m2.forEach((m) => {
+      this.validMoves.push(m);
+    });
+
+    this.handleFilledSlotDestruction(moveMade);
+    this.handleDropSlotDestruction(moveMade);
+    console.log(this.validMoves);
   }
 
   // to be accessed from calculateNewValidMoves only !!!
-  handleDropSlotCreation(move: number[][]) {
+  handleDropSlotCreation(move: number[][]): number[][][] {
     // 2 cases - 1. dropslot created at peg which moved 2. dropslot created at peg which got deleted
     // case 1
     const newMovesCreated: number[][][] = [];
@@ -83,24 +95,23 @@ class ValidMoves {
       }
     });
 
-    newMovesCreated.forEach((e) => {
-      this.validMoves.push(e);
-    });
+    return newMovesCreated;
   }
 
-  handleFilledSlotCreation(move: number[][]) {
+  handleFilledSlotCreation(move: number[][]): number[][][] {
     const newMovesCreated: number[][][] = [];
     // 1. Possible pegToBeMoved 2. Possible pegToBeDeleted
     const neighbors0 = getNeighbors(move[2]);
     const neighborsOfNeighbors0 = getNeighborsOfNeighbors(move[2]);
     // case: 1 -
-    // FIXME: doesnt seem to be workin
     neighbors0.forEach((toBeDel, i) => {
       if (
         this.currentGameState[toBeDel[0]][toBeDel[1]] === PegTypes.FilledSlot &&
         this.currentGameState[neighborsOfNeighbors0[i][0]][neighborsOfNeighbors0[i][1]] ===
           PegTypes.EmptySlot
       ) {
+        console.log("THIS WAS HIT");
+
         newMovesCreated.push([neighborsOfNeighbors0[i], toBeDel, move[2]]);
       }
     });
@@ -113,6 +124,37 @@ class ValidMoves {
       ) {
         newMovesCreated.push([toBeMoved, move[2], neighbors0[j]]);
       }
+    });
+
+    return newMovesCreated;
+  }
+
+  handleFilledSlotDestruction(move: number[][]) {
+    // function for filter below
+    const ifPresent = (cords: number[][]) => {
+      if (
+        isSameCoord(move[0], cords[0]) ||
+        isSameCoord(move[0], cords[1]) ||
+        isSameCoord(move[1], cords[0]) ||
+        isSameCoord(move[1], cords[1])
+      ) {
+        console.log("deleting>>>", cords);
+
+        return false;
+      }
+      // console.log("beep beep");
+
+      return true;
+    };
+    this.validMoves = this.validMoves.filter(ifPresent);
+  }
+
+  handleDropSlotDestruction(move: number[][]) {
+    this.validMoves = this.validMoves.filter((m) => {
+      if (isSameCoord(move[2], m[2])) {
+        return false;
+      }
+      return true;
     });
   }
 
@@ -133,6 +175,15 @@ const getOppositeCords = (k: number): number => {
   } else {
     return 1;
   }
+};
+
+const isSameCoord = (cord1: number[], cord2: number[]) => {
+  if (cord1[0] === cord2[0] && cord1[1] === cord2[1]) {
+    console.log("compared...", cord1, cord2, true);
+    return true;
+  }
+
+  return false;
 };
 
 export default vm1;
